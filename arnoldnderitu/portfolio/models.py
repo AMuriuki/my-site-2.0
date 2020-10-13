179a80158af96c1e2eeb441663c294f496b8b134
arnold <arnoldnderitu@gmail.com>
2020-10-13 16:05:10 +0300

    Integrating with blog templates to wagtail

--- arnoldnderitu/portfolio/models.py
+++ arnoldnderitu/portfolio/models.py
@@ -1,6 +1,9 @@
+from django.db import models
+
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 
 class PorfolioIndexPage(Page):
@@ -8,4 +11,28 @@ class PorfolioIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+    ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        porfoliopages = self.get_children().live().order_by('-first_published_at')
+        context['porfoliopages'] = blogpages
+        return context
+
+
+class PorfolioPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
     ]
